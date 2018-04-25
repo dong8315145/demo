8f556e48ec6c1f7e3d7ea97837245bf7bdb25928
dong8315145 <214808333@qq.com>
2018-04-26 01:19:18 +0800

    lambda表达式有误

--- src/main/java/com/example/demo/controller/ProductCategoryController.java
+++ src/main/java/com/example/demo/controller/ProductCategoryController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.View.BaseView.BaseVO;
 import com.example.demo.View.BaseView.ResultVO;
+import com.example.demo.entity.ProductCategory;
+import com.example.demo.entity.ProductInfo;
 import com.example.demo.service.ProductCategoryService;
 import com.example.demo.service.ProductInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/productcategory")
 public class ProductCategoryController {
     @Autowired
     private ProductCategoryService productCategoryService;
@@ -14,8 +25,17 @@ public class ProductCategoryController {
     private ProductInfoService productInfoService;
 
 
-    public ResultVO list(){
-        return null;
+    @PostMapping("/list")
+    public BaseVO list() {
+        List<ProductInfo> productInfoList = productInfoService.findUpAll();
+        List<Integer> productcategoryTypeList=new ArrayList<>();
+//        List<Integer> productcategoryTypeList =
+//                productInfoList.stream().distinct().
+//                        map(e -> e.getCategoryType()).
+//                        collect(Collectors.toList());
+        List<ProductCategory> list = productCategoryService.findByCategoryTypeIn(productcategoryTypeList);
+
+        return ResultVO.success();
     }
 
 }
