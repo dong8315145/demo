d65b76cec61138a23f738214cd033ba7c05719a0
dong8315145 <214808333@qq.com>
2018-05-12 15:10:46 +0800

    2018/5/12

---
+++ src/test/database/product_category.sql
@@ -0,0 +1,4 @@
+INSERT INTO `product_category` VALUES (1, '女生最爱', 1, '2018-4-24 13:54:40', '2018-4-23 18:03:40');
+INSERT INTO `product_category` VALUES (2, '男生最爱', 2, '2018-4-24 16:06:43', '2018-4-24 16:06:03');
+INSERT INTO `product_category` VALUES (3, '学生最爱', 3, '2018-4-24 16:07:26', '2018-4-24 16:07:24');
+INSERT INTO `product_category` VALUES (4, '大人最爱', 4, '2018-4-24 16:07:23', '2018-4-24 16:07:22');
---
+++ src/test/database/product_info1.sql
@@ -0,0 +1,5 @@
+INSERT INTO `product_info` VALUES ('1', '咖啡', 12.00, 80, '星巴巴', 'www.xxxx.jpe', 1, 1, '2018-4-24 17:34:25', '2018-4-24 17:34:27');
+INSERT INTO `product_info` VALUES ('2', '芒果', 10.00, 70, '台芒', 'www.xxxx.jpe', 0, 2, '2018-4-24 17:34:25', '2018-4-24 17:34:27');
+INSERT INTO `product_info` VALUES ('3', '曲奇', 20.00, 60, '蓝馆', 'www.xxxx.jpe', 0, 2, '2018-4-24 17:34:25', '2018-4-24 17:34:27');
+INSERT INTO `product_info` VALUES ('4', '冰淇淋', 65.00, 10, '五羊', 'www.xxxx.jpe', 0, 2, '2018-4-24 17:34:25', '2018-4-24 17:34:27');
+INSERT INTO `product_info` VALUES ('5', '汉堡', 56.00, 40, '金拱门', 'www.xxxx.jpe', 0, 2, '2018-4-24 17:34:25', '2018-4-24 17:34:27');
